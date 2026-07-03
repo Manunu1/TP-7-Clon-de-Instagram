a2b7bb533a805c8c82fd17baa84088969edaa9c7
Manuel <manuelmandel09@gmail.com>
2026-07-03 12:06:38 -0300

    añadi: actualiza .gitignore, package.json y package-lock.json; agrega controladores para crear y obtener perfil de usuario

---
+++ server/src/controllers/postController.js
@@ -0,0 +1,20 @@
+import pool from "../config/db.js";
+
+export const createPost = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { descripcion, url_imagen } = req.body;
+
+        const result = await pool.query(
+            `INSERT INTO publicaciones (usuario_id, descripcion, url_imagen)
+             VALUES ($1, $2, $3)
+             RETURNING *`,
+            [userId, descripcion, url_imagen]
+        );
+
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error al crear publicación" });
+    }
+};
